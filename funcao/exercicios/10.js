adf3c1f0c55dc2248cee0fabe9465079da20eba0
Jeferson.Silva <jeferson.s.silva1@gmail.com>
2020-04-27 20:20:08 -0300

    11

--- funcao/exercicios/10.js
+++ funcao/exercicios/10.js
@@ -1,10 +1,17 @@
 /* 10) ​ Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true
 ou false. */
 
-// function validaDivisivelPorTres(number){
-//    return number % 3 == 0 ? console.log('True') :  console.log('False')
-// }
-
-validaDivisivelPorTres = number => number % 3 == 0 ? console.log('True') : console.log('False')
+validaDivisivelPorTres = number => number % 3 === 0 ? console.log('True') : console.log('False')
 
 validaDivisivelPorTres(3)
+
+// function validaDivisivelPorTres(number){ 
+//      result = number % 3
+//      if(result == 0){
+//          return console.log('True')
+//      } else {
+//         return console.log('False')
+//      }  
+// }
+
+// validaDivisivelPorTres(3)
