a346360f0420058265ba50336f1fb9d41ec47430
Jeferson.Silva <jeferson.s.silva1@gmail.com>
2020-05-01 14:03:53 -0300

    18

---
+++ funcao/exercicios/18.js
@@ -0,0 +1,38 @@
+// 18)​ Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando
+// switch. Crie um case default que escreva ‘Número fora do intervalo.’
+
+function numeroPorExtenso(numero){
+
+    switch(numero > -1 && numero < 11){
+        case true:
+            switch(numero){
+                case 0:
+                    return console.log('Zero')
+                case 1:
+                    return console.log('Um')
+                case 2:
+                    return console.log('Dois')
+                case 3:
+                    return console.log('Três')
+                case 4:
+                    return console.log('Quatro')
+                case 5:
+                    return console.log('Cinco')
+                case 6:
+                    return console.log('Seis')
+                case 7:
+                    return console.log('Sete')
+                case 8:
+                    return console.log('Oito')
+                case 9:
+                    return console.log('Nove')
+                case 10:
+                    return console.log('Dez')
+            }
+        default:
+            return console.log('Numero invalido')
+    }
+    
+}
+
+numeroPorExtenso(1)
