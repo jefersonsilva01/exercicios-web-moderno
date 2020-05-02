efdbb46562b50dd614ceca124f39cb6cdf6dccd4
Jeferson.Silva <jeferson.s.silva1@gmail.com>
2020-05-01 23:51:36 -0300

    20

---
+++ funcao/exercicios/20.js
@@ -0,0 +1,33 @@
+// 20)​ Crie um programa para informar quais e quantas notas são necessárias para entregar o mínimo de cédulas
+// para um determinado valor informado pelo usuário considerando notas de R$ 100, R$ 50, R$ 10 e R$ 5 e R$ 1.
+// Seu programa deve mostrar apenas as notas utilizadas. Por exemplo, ao solicitar R$18, o programa deve
+// informar apenas a seguinte informação (note que não foram exibidas informações sobre as demais cédulas): 1
+// nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1.
+
+function calculaNotas(valor){
+
+    const notas = [100, 50, 10, 5, 1]
+
+    let contador = 0
+    let nota = 0
+    let resultado = []
+    let i = 0
+
+    while (nota < valor){
+        if(valor >= nota + notas[i]){
+            nota += notas[i]
+            contador++
+            resultado[i] = `${contador} nota(s) de R$ ${notas[i].toFixed(2)}`
+        }  else {
+            i++
+            contador = 0
+        } 
+    }
+    for(i = 0; i < resultado.length; i++){
+        if(typeof resultado[i] == 'string'){
+            console.log(resultado[i])
+        }
+    }
+}
+
+calculaNotas(18)
