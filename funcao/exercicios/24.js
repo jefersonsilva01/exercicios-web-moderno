3ef927771b2715304fbbe19080ee855c27b3a9c7
Jeferson.Silva <jeferson.s.silva1@gmail.com>
2020-05-04 22:50:12 -0300

    24

---
+++ funcao/exercicios/24.js
@@ -0,0 +1,10 @@
+// 24)​ Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+
+let indice = 0
+let posicao = 1
+
+while(indice < 11){
+    console.log(posicao + ' Hello World!')
+    indice++
+    posicao++
+}
