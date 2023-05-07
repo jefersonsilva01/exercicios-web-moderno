d54718aaebfa08017320d943664b0e0df700ab1f
jef <jeferson.s.silva1@gmail.com>
2023-05-07 14:18:37 -0300

    novos exercicios

---
+++ Iniciando com Programação/desafio_area.js
@@ -0,0 +1,6 @@
+// PI = 3.141592
+// PI * raio * raio
+
+let raio =10, PI = 3.141592
+
+console.log(`Área: ${PI * raio * raio}`)
---
+++ Iniciando com Programação/desafio_sentenca_bloco.js
@@ -0,0 +1,20 @@
+bloco1 = () => {
+  let number = 1
+  console.log("Blcoco 1")
+  do { 
+    console.log(number) 
+    number += 2
+  } while (number <=9)
+}
+
+bloco2 = () => {
+  let number = 2
+  console.log("Blcoco 2")
+  do {
+    console.log(number)
+    number += 2
+  } while (number <=10)
+}
+
+bloco1()
+bloco2()
