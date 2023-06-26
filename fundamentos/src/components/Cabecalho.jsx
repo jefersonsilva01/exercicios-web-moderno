f930079db52ed4834e3f25ff0a0b60cdba071dc1
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2023-06-26 18:23:00 -0300

    change

--- fundamentos/src/components/Cabecalho.jsx
+++
@@ -1,7 +0,0 @@
-export default function Cabecalho(props) {  
-  return (
-    <header>
-      <h1>{props.titulo}</h1>
-    </header>
-  )
-}
--- fundamentos/src/pages/api/clientes/[codigo].js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const codigo = req.query.codigo
-  res.status(200).json({
-    id: codigo,
-    nome: `Maria ${codigo}`,
-    email: `maria${codigo}@email.com`
-  })
-}
