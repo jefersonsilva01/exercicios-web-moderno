c2afaf4082ffb675c611a8eb5950f2868784ab8a
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2023-06-26 18:20:49 -0300

    change

---
+++ fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout"
+
+export default function Jsx() {
+  return (
+    <Layout titulo="Entendendo Jsx">
+      <div>
+        <h1>JSX é um conceito Central</h1>
+      </div>
+    </Layout>
+  )
+}
