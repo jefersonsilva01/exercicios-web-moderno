d3d17b08c90814d20e64709d28c1202c98c2f053
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2023-07-10 17:13:06 -0300

    Iniciado frontend

---
+++ knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="admin-page">
+    <PageTitle icon="fa fa-cogs" main="Administração do Sistema" 
+      sub="Cadastro & Cia" />
+  </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle.vue'
+
+export default {
+  name: 'AdminPages',
+  components: { PageTitle }
+}
+</script>
+
+<style>
+
+</style>
